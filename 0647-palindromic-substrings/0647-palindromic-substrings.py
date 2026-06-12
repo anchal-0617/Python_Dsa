0699eaa7bdc88d966fc4f70a14d41960521e2126
anchal-0617 <anchal0617@gmail.com>
2026-06-12 17:51:24 +0530

    Time: 389 ms (19.84%), Space: 20 MB (14.7%) - LeetHub

---
+++ 0647-palindromic-substrings/0647-palindromic-substrings.py
@@ -0,0 +1,34 @@
+class Solution(object):
+    def countSubstrings(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+
+
+
+
+        n = len(s)
+
+        t = [[False] * n for _ in range(n)]
+        count = 0
+
+        for l in range (1,n+1):
+            for i in range (n-l+1):
+                j = i+l-1
+
+                if (i==j):
+                    t[i][j] = True
+
+                elif ( i+1 == j):
+                    t[i][j] = s[i] == s[j]
+
+                else:
+                    t[i][j] =  s[i] == s[j] and t[i+1][j-1]
+
+                if t[i][j]:
+                    count = count+1
+
+        return count         
+
+        
