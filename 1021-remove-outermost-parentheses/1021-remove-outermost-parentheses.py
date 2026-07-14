368a1118e0d1fee51ff766f83517259e5e10610f
anchal-0617 <anchal0617@gmail.com>
2026-07-14 11:51:01 +0530

    Time: 3 ms (94%), Space: 12.5 MB (64.51%) - LeetHub

---
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def removeOuterParentheses(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+
+
+        ans = ""
+        count = 0
+
+        for ch in s:
+            if ch in "(" :
+
+                if count > 0:
+                    ans += ch
+                count += 1
+
+            else :
+
+                count -= 1
+
+                if count >0:
+                    ans += ch
+
+        return ans            
+
+        
