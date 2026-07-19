c3e324054bf60cc458b625e78f766d800e618619
anchal-0617 <anchal0617@gmail.com>
2026-07-19 11:16:32 +0530

    Time: 19 ms (8.24%), Space: 17.1 MB (97.97%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.py
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.py
@@ -5,29 +5,52 @@
 #         self.left = left
 #         self.right = right
 class Solution(object):
+
+    def height(self , root):
+        if not root:
+            return 0
+        leftH =  self.height(root.left)
+        rightH = self.height(root.right)
+        return max(leftH , rightH) + 1
+
     def isBalanced(self, root):
         """
         :type root: Optional[TreeNode]
         :rtype: bool
         """
 
-        def dfs(root):
-            if root is None:
-                return 0
+        # def dfs(root):
+        #     if root is None:
+        #         return 0
 
-            left = dfs(root.left)
-            if left == -1:
-                return -1
-            right = dfs(root.right)
-            if right == -1:
-                return -1
+        #     left = dfs(root.left)
+        #     if left == -1:
+        #         return -1
+        #     right = dfs(root.right)
+        #     if right == -1:
+        #         return -1
 
-            if abs(left-right) >1:
-                return -1
+        #     if abs(left-right) >1:
+        #         return -1
 
 
-            height = 1 + max(left, right)
-            return height
+        #     height = 1 + max(left, right)
+        #     return height
 
-        return dfs(root)!= -1
+        # return dfs(root)!= -1
+        def solve(root):
+            if not root:
+                return True
+
+            leftH = self.height(root.left)
+
+            rightH = self.height(root.right)
+
+            if abs(leftH - rightH) > 1:
+                return False
+
+            return solve(root.left) and solve(root.right)
+
+        return solve(root)     
         
+
