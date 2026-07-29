7a14ef2db9ce8937511e30b56ea67ae6a0ac1da4
anchal-0617 <anchal0617@gmail.com>
2026-07-29 10:10:42 +0530

    Time: 9 ms (30.53%), Space: 12.4 MB (56.84%) - LeetHub

---
+++ 1252-cells-with-odd-values-in-a-matrix/1252-cells-with-odd-values-in-a-matrix.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def oddCells(self, m, n, indices):
+        """
+        :type m: int
+        :type n: int
+        :type indices: List[List[int]]
+        :rtype: int
+        """
+        matrix = [[0] * n for _ in range(m)]
+
+        for r , c in indices:
+
+            for j in range(n):
+
+                matrix[r][j] += 1
+                
+            for i in range(m):
+
+                matrix[i][c] += 1
+
+        count = 0
+
+        for i in range(m):
+            for j in range(n):
+
+                if matrix[i][j] %2 == 1:
+                    count +=1
+        return count                        
+
+        
