5b23526eb702049ad954b5d1a24ae38ec236ef07
anchal-0617 <anchal0617@gmail.com>
2026-07-29 10:15:35 +0530

    Time: 0 ms (100%), Space: 12.5 MB (24.21%) - LeetHub

--- 1252-cells-with-odd-values-in-a-matrix/1252-cells-with-odd-values-in-a-matrix.py
+++ 1252-cells-with-odd-values-in-a-matrix/1252-cells-with-odd-values-in-a-matrix.py
@@ -6,25 +6,47 @@ class Solution(object):
         :type indices: List[List[int]]
         :rtype: int
         """
-        matrix = [[0] * n for _ in range(m)]
+        # matrix = [[0] * n for _ in range(m)]
 
-        for r , c in indices:
+        # for r , c in indices:
 
-            for j in range(n):
+        #     for j in range(n):
 
-                matrix[r][j] += 1
+        #         matrix[r][j] += 1
                 
-            for i in range(m):
+        #     for i in range(m):
 
-                matrix[i][c] += 1
+        #         matrix[i][c] += 1
+
+        # count = 0
+
+        # for i in range(m):
+        #     for j in range(n):
+
+        #         if matrix[i][j] %2 == 1:
+        #             count +=1
+        # return count    
+
+        #Instead of making matrix we directly count,
+
+        row = [0]*m
+        col = [0]*n
+        for r,c in indices :
+            row[r] +=1
+
+            col[c] +=1
 
         count = 0
 
         for i in range(m):
             for j in range(n):
 
-                if matrix[i][j] %2 == 1:
-                    count +=1
-        return count                        
+                if (row[i] + col[j]) %2 == 1:
+                    count+=1
+
+        return count                
+
+
+
 
         
