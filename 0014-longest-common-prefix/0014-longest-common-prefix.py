5c395350373869b1dd7257fb8046f70099cea609
anchal-0617 <anchal0617@gmail.com>
2026-09-06 16:24:38 +0530

    Time: 0 ms (100%), Space: 12.4 MB (70.3%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -7,14 +7,15 @@ class Solution(object):
 
         prefix = strs[0]
 
-        for i in range(1, len(strs)):
+        for i in range(1,len(strs)):
+
             while not strs[i].startswith(prefix):
 
+            
                 prefix = prefix[:-1]
 
-                if prefix == "":
-
+                if prefix == "" :
                     return ""
 
-        return prefix            
+        return prefix        
         
