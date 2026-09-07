cdcaa1d77b94edfaee507cbd61b0242db6b2d62b
anchal-0617 <anchal0617@gmail.com>
2026-09-07 14:40:42 +0530

    Time: 0 ms (100%), Space: 13.8 MB (43.85%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -11,5 +11,8 @@ class Solution(object):
             if nums[i] != nums[j]:
                 i+=1
                 nums[i] = nums[j]
-        return i+1        
+        return i+1
+
+                
+
         
