0d3fabcc9491bfac9aa24f81ca62b0fe8958bb4b
anchal-0617 <anchal0617@gmail.com>
2026-09-08 10:46:10 +0530

    Time: 0 ms (100%), Space: 12.3 MB (55.66%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -7,10 +7,30 @@ class Solution(object):
 
         n = len(digits)
 
-        for i in range(n-1 , -1 , -1):
-            if digits[i] < 9:
-                digits[i] += 1
-                return digits
-            digits[i] = 0
-        return [1] + digits        
+        # for i in range(n-1 , -1 , -1):
+        #     if digits[i] < 9:
+        #         digits[i] += 1
+        #         return digits
+        #     digits[i] = 0
+        # return [1] + digits
+        num = 0
+        for i in range(n):
+            
+            digit = int(digits[i])
+
+            num = num * 10 + digit
+        num += 1    
+        arr = []
+        while num > 0:
+            digit = num % 10
+
+            arr.append(digit)
+            num //= 10
+        arr.reverse()
+
+        return arr    
+
+
+
+
         
