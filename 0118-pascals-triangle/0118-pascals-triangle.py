d7a19522acd33ac3a05d8b47374e3591df182c7e
anchal-0617 <anchal0617@gmail.com>
2026-09-08 16:10:50 +0530

    Time: 0 ms (100%), Space: 12.3 MB (94.85%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.py
+++ 0118-pascals-triangle/0118-pascals-triangle.py
@@ -7,14 +7,18 @@ class Solution(object):
 
         ans = []
         n = numRows
-        for n in range(1 , n +1):
+        for n in range( n ):
             row = [1]
 
         
             for i in range(1,n):
-                value = row[-1] * (n-i) // i
+                # value = row[-1] * (n-i) // i
 
-                row.append(value)
+                row.append(ans[n-1][i-1]+ ans[n-1][i])
+
+            if n>0: 
+
+                row.append(1)   
             ans.append(row)    
         return ans    
         
