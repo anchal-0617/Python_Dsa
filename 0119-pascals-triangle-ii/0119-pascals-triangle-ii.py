559eb42703b89e49aff67598371e13934606dd3a
anchal-0617 <anchal0617@gmail.com>
2026-09-08 16:13:07 +0530

    Time: 1 ms (20.9%), Space: 12.4 MB (19.25%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
@@ -5,12 +5,29 @@ class Solution(object):
         :rtype: List[int]
         """
         
-        ans = [1]
-        n = rowIndex+1
+        # ans = [1]
+        # n = rowIndex + 1
 
-        for i in range(1,n):
-            value = ans[-1] * (n-i) // i
+        # for i in range(1,n):
+        #     value = ans[-1] * (n-i) // i
 
-            ans.append(value)
+        #     ans.append(value)
 
-        return ans     
+        # return ans     
+
+        ans = []
+        n = rowIndex + 1
+        for n in range( n ):
+            row = [1]
+
+        
+            for i in range(1,n):
+                # value = row[-1] * (n-i) // i
+
+                row.append(ans[n-1][i-1]+ ans[n-1][i])
+
+            if n>0: 
+
+                row.append(1)   
+            ans.append(row)    
+        return ans[-1] 
