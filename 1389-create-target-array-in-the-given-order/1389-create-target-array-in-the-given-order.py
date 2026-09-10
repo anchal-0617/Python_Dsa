5048f2312a452caeccd6b936848cf8e620fc95c6
anchal-0617 <anchal0617@gmail.com>
2026-09-10 15:17:59 +0530

    Time: 0 ms (100%), Space: 12.3 MB (90.57%) - LeetHub

---
+++ 1389-create-target-array-in-the-given-order/1389-create-target-array-in-the-given-order.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def createTargetArray(self, nums, index):
+        """
+        :type nums: List[int]
+        :type index: List[int]
+        :rtype: List[int]
+        """
+        ans = []
+        for i in range(len(nums)):
+
+            
+
+                ans.insert(index[i] , nums[i])
+
+
+
+        return ans
