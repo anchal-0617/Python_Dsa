2d20d50c8a4a403974240209f911123c4c7f3c8c
anchal-0617 <anchal0617@gmail.com>
2026-09-12 18:22:52 +0530

    Time: 883 ms (14.61%), Space: 13.5 MB (10.74%) - LeetHub

--- 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.py
+++ 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.py
@@ -6,25 +6,77 @@ class Solution(object):
         :rtype: List[int]
         """
 
-        i = len(num) -1
+        # i = len(num) -1
 
-        carry = 0
-        ans = []
+        # carry = 0
+        # ans = []
 
-        while i >= 0 or k > 0 or carry:
+        # while i >= 0 or k > 0 or carry:
 
-            digit = carry
+        #     digit = carry
 
-            if i>= 0:
+        #     if i>= 0:
 
-                digit += num[i]
+        #         digit += num[i]
 
-                i-=1
-            digit += k%10
+        #         i-=1
+        #     digit += k%10
 
-            k//=10   
-            ans.append(digit%10)
-            carry = digit//10
+        #     k//=10   
+        #     ans.append(digit%10)
+        #     carry = digit//10
 
-        return ans[::-1]     
+        # return ans[::-1] 
+
+
+        nums = 0
+
+        for digit in num:
+
+            nums = nums * 10 + digit
+
+        nums = nums + k
+
+
+        arr = []
+
+        while(nums> 0):
+
+            digit = nums % 10
+            arr.append(digit)
+
+            nums = nums//10
+
+        return arr[::-1]        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         
