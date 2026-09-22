8cd917911a45cd37f2d41de4c0bf6b73b2ed9534
anchal-0617 <anchal0617@gmail.com>
2026-09-22 10:59:27 +0530

    Time: 0 ms (100%), Space: 12.4 MB (18.01%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.py
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.py
@@ -7,9 +7,9 @@ class Solution(object):
         good = 0
         for i in range(len(nums)):
 
-            for j in range(i , len(nums)):
+            for j in range(i+1 , len(nums)):
 
-                if nums[i] == nums[j] and i < j:
+                if nums[i] == nums[j]:
 
                     good +=1
 
